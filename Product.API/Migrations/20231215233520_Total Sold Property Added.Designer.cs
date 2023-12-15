6c0b8a41093c599ebcedd06a748bc761b751aa21
Imran Khan <imrankhandev7@gmail.com>
2023-12-16 04:36:13 +0500

    migrations

--- Product.API/Migrations/20231215233520_Total Sold Property Added.Designer.cs
+++ Product.API/Migrations/20231215233520_Total Sold Property Added.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Products.API.Data;
 
@@ -10,9 +11,10 @@ using Products.API.Data;
 namespace Products.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231215233520_Total Sold Property Added")]
+    partial class TotalSoldPropertyAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
